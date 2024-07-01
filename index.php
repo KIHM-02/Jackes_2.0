63ff50bebf818c358498d51378eb772dd40e38d0
KIHM-02 <monteroisaac33@gmail.com>
2024-06-30 21:50:29 -0600

    Added the html landing page and the style of the nav menu

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/init-border.css">
+    <link rel="stylesheet" href="css/color.css">
+    <link rel="stylesheet" href="css/nav.css">
+</head>
+<body>
+    <nav class="nav-bar">
+        <ul class="ul-left">
+            <li>
+                <label hidden for="menu-hamb">Menu</label>
+                <input type="checkbox" hidden id="menu-hamb">
+            </li>
+            <li><a class="title" href="index.php">Jackes</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+        <ul class="ul-right">
+            <li><a href="login.php">Iniciar sesión</a></li>
+        </ul>
+    </nav>
+
+    <header>
+        <div>
+            <h1>Jackes</h1>
+            <p>Las mejores maquinas para pasar el tiempo</p>
+            <button>Iniciar sesión</button>
+        </div>
+    </header>
+
+    <section>
+        <div>
+            <h2>Misión</h2>
+            <p>Nuestra misión es hacer que todas las tiendas puedan tener
+                una maquina de juego donde puedan disfrutar un rato inolvidable 
+            </p>
+        </div>
+        <div>
+            <h2>Visión</h2>
+            <p>Hacer que toda la gente tenga oportunidad de divertirse un rato jugando
+                 y obteniendo premios</p>
+        </div>
+    </section>
+    <section>
+        <h2>Contacto</h2>
+        <p>Numero de celular: 33123456789</p>
+        <p>Correo electronico: correo@dominio.ext</p>
+    </section>
+
+</body>
+</html>
