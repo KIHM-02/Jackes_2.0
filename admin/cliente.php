15b8eeec7b1857c9173942975b185c575f811cae
KIHM-02 <monteroisaac33@gmail.com>
2024-08-09 00:37:00 -0600

    Added delete to cliente

--- admin/cliente.php
+++ admin/cliente.php
@@ -1,4 +1,51 @@
 <?php include("template/header.php") ?>
+
+<?php
+        require_once("config/conexion.php");
+
+        $conection = new Conexion(); //var para instanciar clase usuario
+        $accion = ($_POST)? $_POST['accion']: "all";
+
+        $idUsr  = ($_POST) && !empty($_POST['txtId'])? intval($_POST['txtId']): null;
+        $name      = ($_POST) && !empty($_POST['txtName'])? $_POST['txtName']: null;
+        $municipio = ($_POST) && !empty($_POST['txtIdMuni'])? intval($_POST['txtIdMuni']): null;
+
+        $arrayFilters = [];
+        $arrayMunicipio = [];
+
+        $voidCamp = false;
+
+        switch($accion)
+        {
+            case "delete_muni":
+                    $arrayMunicipio = ["idMuni" => $municipio];
+                    $conection->useDelete("municipio", $arrayMunicipio);
+                    $accion = "all"; //reseteamos la variable accion para mostrar los registros usuario
+                break;
+
+            case "delete_user":
+                    $arrayFilters = ["idUsr" => $idUsr];
+                    $conection->useDelete("trabajador", $arrayFilters);
+                    $accion = "all"; //reseteamos la variable accion para mostrar los registros usuario
+                break;
+
+            case "add_municipio":
+                    $tipo = (!empty($_POST['txtTipoMuni']))? trim($_POST['txtTipolmuni']): null;
+                    if($tipo === null)
+                    {
+                        $voidCamp = true;
+                    }
+                    else
+                    {
+                        $arraymunicipio = ["tipomuni" => $tipo];
+                        $conection->useInsert("municipio", $arrayMunicipio);
+                    }
+                    $accion = "all"; //reseteamos la variable accion para mostrar los registros usuario
+                break;
+        }
+        
+    ?>
+
     <header>
         <h2 class="subtitle header-elements">Panel Clientes</h2>
 
@@ -34,33 +81,49 @@
         <label for = "chkTable" class ="btn-black-header table-buttons">Desplegar</label>
         <input type="checkbox" name="chkTable" id="chkTable" class ="chkTable table-buttons">
 
-        <div class ="border-table space-top">
+        <div class ="hide-component space-top">
             <table class = "table">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Municipio</th>
-                        <th>Modificar</th>
                         <th>Eliminar</th>
                     </tr>
                 </thead>
 
                 <tbody>
+
+                <?php
+                    $arrayMunicipio = ["idMuni" => null];
+                    $municipioData = $conection->getdata("municipio", $arrayMunicipio);
+
+                    foreach($municipioData as $municipios)
+                    {
+                 ?>
                     <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
+                        <td><?php echo htmlspecialchars($municipios['idMuni']); ?></td>
+                        <td><?php echo htmlspecialchars($municipios['municipio']);?></td>
+
+                        <td>
+
+                            <form method="POST">
+                                <input type="hidden" name="txtIdMuni" value = "<?php echo htmlspecialchars ($municipios ['idMuni']); ?>">
+                                <button type ="submit" name = "accion" value ="delete_muni">Eliminar</button>
+                            </form>    
+                        </td>
                     </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
+                <?php } ?>
                 </tbody>
             </table>
         </div>
+
+        <form method="post"> class ="space-top form-table">
+            <div class ="div=form=inputs">
+
+        </form>
+
+
+
     </section>
 
     <section class = "section-title">
