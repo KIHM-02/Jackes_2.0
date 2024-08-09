0f1f439c8eb7cc1855478ee1477c2b529c4b2453
KIHM-02 <monteroisaac33@gmail.com>
2024-08-09 03:39:02 -0600

    Added the insert to vehiculo_registro

--- admin/entityRegistration/vehiculo_registro.php
+++ admin/entityRegistration/vehiculo_registro.php
@@ -1,9 +1,65 @@
 <?php include("../template/header_registro.php"); ?>
+    <?php
+        require_once("../config/conexion.php");
+
+        $conection = new Conexion(); //var para instanciar clase usuario
+        $arrayVehi = [];
+        $arrayWorker =[];
+        $arrayValues = [];
+        $voidValues = false;
+
+        if($_POST)
+        {
+            $fecha     = (!empty($_POST['txtDate']))? trim($_POST['txtDate']): null;
+            $carId     = (!empty($_POST['txtCarId']))? trim($_POST['txtCarId']): null;
+            $usrId     = (!empty($_POST['txtWorkerId']))? trim($_POST['txtWorkerId']): null;
+            $distancia = (!empty($_POST['txtDistance']))? trim($_POST['txtDistance']): null;
+            $dia = null;
+            $mes = null; 
+            $anio = null;
+
+            if($fecha !== null)
+            {
+                list($anio, $mes, $dia) = explode('-', $fecha);
+            }
+            
+            $arrayValues = [
+                "dia" => $dia,
+                "mes" => $mes,
+                "anio" => $anio,
+                "distanciaCond" => $distancia,
+                "idUsr" => $usrId,
+                "idVehi" => $carId 
+            ];
+
+            foreach($arrayValues as $camp => $value)
+            {
+                if($value === null)
+                {
+                    $voidValues = true;
+                    break;
+                }
+            }
+    
+            if($voidValues === false)
+            {
+                $conection->useInsert("conduce", $arrayValues);
+            }
+        }
+    ?>
     <header>
-        <h2 class ="subtitle">Agregar vehiculo</h2>
+        <h2 class ="subtitle">Agregar recorrido</h2>
     </header>
 
-    <form action="post" class="form-register">
+    <?php  //Mostrar advertencia de campos vacios
+        if($voidValues)
+        { ?>
+            <div class ="warning-box">
+                <p>Hay campos de informacion vacios, no se pudo procesar la peticion</p>
+            </div>
+    <?php } ?>
+
+    <form method="post" class="form-register">
         <section class ="divider">
             <div class ="section-title"><h2>Fecha</h2></div>
             <div class ="section-inputs">
@@ -20,14 +76,34 @@
                 <article class ="inputs">
                     <label for="txtCarId">Id vehiculo</label>
                     <select name="txtCarId" id="txtCarId">
-                        <option value="Hello">GTR</option>
+                    <?php 
+                        $arrayVehi = ["idVehi" => null];
+                        $vehiData = $conection->getData("vehiculo", $arrayVehi);
+
+                        foreach($vehiData as $vehiculo)
+                        {
+                    ?>
+                        <option value="<?php echo htmlspecialchars($vehiculo['idVehi']); ?>">
+                            <?php echo htmlspecialchars($vehiculo['idVehi']."-".$vehiculo['modeloVehi']); ?>
+                        </option>            
+                <?php   } ?>
                     </select>
                 </article>
 
                 <article class ="inputs">
                     <label for="txtWorkerId">Id trabajador</label>
                     <select name="txtWorkerId" id="txtWorkerId">
-                        <option value="Hello">1-Ana</option>
+                    <?php 
+                        $arrayWorker = ["idUsr" => null];
+                        $userData = $conection->getData("trabajador", $arrayWorker);
+
+                        foreach($userData as $user)
+                        {
+                    ?>
+                        <option value="<?php echo htmlspecialchars($user['idUsr']); ?>">
+                            <?php echo htmlspecialchars($user['idUsr']."-".$user['nombreUsr']); ?>
+                        </option>            
+                <?php   } ?>
                     </select>
                 </article>
 
