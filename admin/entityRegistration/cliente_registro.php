1dbfdc5489bc80dc676abcea796625f06e0b46b9
KIHM-02 <monteroisaac33@gmail.com>
2024-08-09 03:40:20 -0600

    Added the method to insert in cliente_registro

--- admin/entityRegistration/cliente_registro.php
+++ admin/entityRegistration/cliente_registro.php
@@ -1,10 +1,69 @@
 <?php include("../template/header_registro.php"); ?>
 
+    <?php
+        require_once("../config/conexion.php");
+
+        $conection = new Conexion(); //var para instanciar clase usuario
+        $arrayRol = [];
+        $arrayValues = [];
+        $incorrectPwd = false;
+        $voidValues = false;
+
+        if($_POST)
+        {
+            $name          = (!empty($_POST['txtName']))? trim($_POST['txtName']): null;
+            $apePat        = (!empty($_POST['txtPat']))? trim($_POST['txtPat']): null;
+            $apeMat        = (!empty($_POST['txtMat']))? trim($_POST['txtMat']): null;
+            $tel           = (!empty($_POST['txtTel']))? trim($_POST['txtTel']): null;
+            $domicilio     = (!empty($_POST['txtDom']))? trim($_POST['txtDom']): null;
+            $colonia       = (!empty($_POST['txtColonia']))? trim($_POST['txtColonia']): null;
+            $numExterior   = (!empty($_POST['txtNumExt']))? trim($_POST['txtNumExt']): null;
+            $numInterior   = (!empty($_POST['txtNumInt']))? trim($_POST['txtNumInt']): null;
+            $municipio     = (!empty($_POST['txtMunicipioId']))? intval(trim($_POST['txtMunicipioId'])): null;
+
+            $arrayValues = [
+                "nombreClie" => $name,
+                "apePatClie" => $apePat,
+                "apeMatClie"=> $apeMat,
+                "coloniaClie"=> $colonia,
+                "direccionClie" => $domicilio,
+                "telefonoClie" => $tel,
+                "numInteriorClie" => $numInterior,
+                "numExteriorClie" => $numExterior,
+                "idMunicipio" => $municipio
+            ];
+
+            foreach($arrayValues as $camp => $value)
+            {    
+                if($value === null)
+                {
+                    $voidValues = true;
+                    break;
+                }
+            }
+    
+            if($voidValues === false)
+            {
+                $conection->useInsert("cliente", $arrayValues);
+            }
+        }
+    ?>
+
     <header>
         <h2 class ="subtitle">Agregar Cliente</h2>
     </header>
 
-    <form action="post" class="form-register">
+    
+    <?php  //Mostrar advertencia de campos vacios
+        if($voidValues)
+        { ?>
+            <div class ="warning-box">
+                <p>Hay campos de informacion vacios, no se pudo procesar la peticion</p>
+            </div>
+    <?php } ?>
+
+
+    <form method="post" class="form-register">
         <section class ="divider">
             <div class ="section-title"><h2>Datos Personales</h2></div>
             <div class ="section-inputs">
@@ -31,29 +90,40 @@
             <div class ="section-title"><h2>Ubicación</h2></div>
             <div class ="section-inputs">  
                 <article class ="inputs">
-                    <label for="">Domicilio</label>
-                    <input type="text" id="" name = "">
+                    <label for="txtDom">Domicilio</label>
+                    <input type="text" id="txtDom" name = "txtDom">
                 </article>
                 
                 <article class ="inputs">
-                    <label for="">Numero Exterior</label>
-                    <input type="text" id="" name = "">
+                    <label for="txtNumExt">Numero Exterior</label>
+                    <input type="text" id="txtNumExt" name = "txtNumExt">
                 </article>
 
                 <article class ="inputs">
-                    <label for="">Numero Interior</label>
-                    <input type="text" id="" name = "">
+                    <label for="txtNumInt">Numero Interior</label>
+                    <input type="text" id="txtNumInt" name = "txtNumInt" value="s/n">
                 </article>
 
                 <article class ="inputs">
-                    <label for="">Colonia</label>
-                    <input type="text" id="" name = "">
+                    <label for="txtColonia">Colonia</label>
+                    <input type="text" id="txtColonia" name = "txtColonia">
                 </article>
 
                 <article class ="inputs">
                     <label for="txtMunicipioId">Id Municipio</label>
                     <select name="txtMunicipioId" id="txtMunicipioId">
-                        <option value="Hello">1-Tlaquepaque</option>
+                    <?php 
+                        $arrayMuni = ["idMuni" => null];
+                        $muniData = $conection->getData("municipio", $arrayMuni);
+
+                        foreach($muniData as $data)
+                        {
+                    ?>
+                        <option value="<?php echo htmlspecialchars ($data['idMuni']); ?>">
+                            <?php echo htmlspecialchars ($data['idMuni']); ?>-
+                            <?php echo htmlspecialchars ($data['municipio']); ?>
+                        </option>
+                    <?php } ?>
                     </select>
                 </article>
             </div>
